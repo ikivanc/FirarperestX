f60bc9964006c402c94188f102be23e405ba16ef
Ibrahim KIVANC <ikivanc@hotmail.com>
2016-11-10 17:04:55 +0300

    Application Icon updated

--- FirarperestX/FirarperestX.Droid/MainActivity.cs
+++ FirarperestX/FirarperestX.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace FirarperestX.Droid
 {
-    [Activity(Label = "FirarperestX", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = " Firarperest", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
