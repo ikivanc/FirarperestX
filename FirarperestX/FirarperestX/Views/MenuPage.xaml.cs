37fec3b4dc66f807ae2bc082ff575371eb36d4f9
Ibrahim KIVANC <ikivanc@hotmail.com>
2016-11-10 16:27:21 +0300

    new pages, menu, navigations, country on page details are added.

--- FirarperestX/FirarperestX/Views/MenuPage.xaml.cs
+++ FirarperestX/FirarperestX/Views/MenuPage.xaml.cs
@@ -14,9 +14,32 @@ namespace FirarperestX.Views
         {
             InitializeComponent();
 
-            Title = "Programcıdan Programcıya";
+            Title = "Firarperest";
+            listMenu.ItemsSource = new List<String> { "Photos" , "Show by Country", "Show on Map", "Go to Website"};    
+        }
 
-    
+        private async void onMenuSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            string selectedItem = listMenu.SelectedItem as String;
+
+            if (selectedItem == "Photos")
+            {
+                await this.Navigation.PushModalAsync(new Views.Home());
+            }
+            else if (selectedItem == "Show by Country")
+            {
+                await this.Navigation.PushModalAsync(new Views.PageByCountry());
+            }
+            else if (selectedItem == "Show on Map")
+            {
+                await this.Navigation.PushModalAsync(new Views.PageOnMap());
+            }
+            else if (selectedItem == "Go to Website")
+            {            
+                Device.OpenUri(new Uri("http://ikivanc.tumblr.com"));            
+            }
+
+            listMenu.SelectedItem = null;
         }
     }
 }
---
+++ FirarperestX/FirarperestX/Views/PageByCountry.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FirarperestX.Views
+{
+    public partial class PageByCountry : ContentPage
+    {
+        public PageByCountry()
+        {
+            InitializeComponent();
+
+            Photoset ps = new Photoset();
+            listviewPhotos.ItemsSource = ps.getByCountryGrouped();
+
+        }
+
+
+        public async void onTapped(object sender, ItemTappedEventArgs e)
+        {
+            var list = (ListView)sender;
+            var selectedphoto = e.Item as Photoset;
+
+            (Application.Current as App).selectedPhotoset = selectedphoto;
+
+            await this.Navigation.PushModalAsync(new Views.DetailPage());
+
+            list.SelectedItem = null;
+        }
+    }
+}
